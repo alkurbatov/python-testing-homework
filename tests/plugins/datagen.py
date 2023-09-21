5060680eafe89e7b2320d73d317c43e31100b75c
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2023-09-21 22:10:18 +0300

    Apply better seed logic
    * Use pytest-randomly plugin in mimesis instead of manually passing seed.
    * Allow to specify seed as runtime via --randomly-seed option.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- tests/plugins/datagen.py
+++ tests/plugins/datagen.py
@@ -1,17 +1,34 @@
+import random
+from typing import Final
+
 import pytest
 from mimesis.locales import Locale
-from mimesis.random import Random
 from mimesis.schema import Field
 
+SEED_RAND_BITS: Final = 32
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    """Create or restore random value to be used as seed during tests."""
+    seed_value = config.getoption('randomly_seed')
+    default_seed = random.Random().getrandbits(SEED_RAND_BITS)
+
+    if seed_value == 'last':
+        seed = config.cache.get(  # type: ignore[union-attr]
+            'randomly_seed',
+            default_seed,
+        )
+    elif seed_value == 'default':
+        seed = default_seed
+    else:
+        seed = seed_value
+
+    config.cache.set('randomly_seed', seed)  # type: ignore[union-attr]
+
+    config.option.randomly_seed = seed
+
 
 @pytest.fixture(scope='session')
-def seed() -> int:
-    """Create random value to be used as seed in mimesis."""
-    max_int = 1000000
-    return Random().randint(a=1, b=max_int)
-
-
-@pytest.fixture(scope='session')
-def mf(seed: int) -> Field:
+def mf() -> Field:
     """Random data provider."""
-    return Field(locale=Locale.RU, seed=seed)
+    return Field(locale=Locale.RU)
