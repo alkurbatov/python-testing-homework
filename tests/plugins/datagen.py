5c815dd00b17ca668e66b50b6dbdde2e9f4d57c1
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2023-09-28 11:37:33 +0300

    Remove randomly seed implementation
    As it is already implemented in pytest-randomly.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- tests/plugins/datagen.py
+++ tests/plugins/datagen.py
@@ -1,31 +1,6 @@
-import random
-from typing import Final
-
 import pytest
 from mimesis.locales import Locale
 from mimesis.schema import Field
-
-SEED_RAND_BITS: Final = 32
-
-
-def pytest_configure(config: pytest.Config) -> None:
-    """Create or restore random value to be used as seed during tests."""
-    seed_value = config.getoption('randomly_seed')
-    default_seed = random.Random().getrandbits(SEED_RAND_BITS)
-
-    if seed_value == 'last':
-        seed = config.cache.get(  # type: ignore[union-attr]
-            'randomly_seed',
-            default_seed,
-        )
-    elif seed_value == 'default':
-        seed = default_seed
-    else:
-        seed = seed_value
-
-    config.cache.set('randomly_seed', seed)  # type: ignore[union-attr]
-
-    config.option.randomly_seed = seed
 
 
 @pytest.fixture(scope='session')
